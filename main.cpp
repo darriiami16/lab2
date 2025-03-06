9c1b816dec5abee9a4e619f3cc634e26f77ebef5
Dasha <khatsiolad@gmail.com>
2025-03-06 21:59:40 +0200

    init commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "enter (int): ";
+    cin >> a;
+    cout << "test" << endl;
+    cout << "a=" << a << endl;
+    Sleep(1000);
+    printf("Hello, world2!\n");
+
+    system("pause"); // Для Windows, чтобы консоль не закрывалась сразу
+    return 0;
+}
